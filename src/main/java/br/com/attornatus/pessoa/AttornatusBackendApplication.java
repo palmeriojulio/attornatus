35c261e9ebedc8b09435e5af004dc01c13ee77f5
Palmério <palmerio_julio@hotmail.com>
2023-01-03 11:56:42 -0300

    Commit Inicial

---
+++ src/main/java/br/com/attornatus/pessoa/AttornatusBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.pessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttornatusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttornatusBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/attornatus/pessoa/AttornatusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.pessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttornatusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
